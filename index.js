90ffa96ebd410a4e49c8a32a012ba2dc10c7d0f3
payalgol <payalgol.pg@gmail.com>
2025-05-05 17:22:56 +0530

    fix:commit

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+// app.use(express.static("public"));
 
 dbConnections();
 
